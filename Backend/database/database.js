b465bb3ee7014fc6b0612648f913327387365a9c
Yogen Shrestha <yogenstha520@gmail.com>
2025-12-04 15:47:23 +0545

    added mongoDb atlas url

--- Backend/database/database.js
+++ Backend/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectToDatabase = async () => {
-    const connection = await mongoose.connect("mongodb://127.0.0.1:27017/NoteApp");
+    const connection = await mongoose.connect("mongodb+srv://yogenstha520_db_user:cdgjslWrLtE8zRnn@noteapp.p99uakr.mongodb.net/?appName=NoteApp");
     console.log('MongoDB is connected')
     return connection   
 }
